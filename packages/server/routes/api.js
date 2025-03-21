7f0ddc330ddd8c8b7b18b5ef983ac44d5611c4f9
Tom Nielsen <tanielsen@gmail.com>
2025-03-21 21:34:39 +0000

    api

--- packages/server/routes/api.js
+++ packages/server/routes/api.js
@@ -15,7 +15,7 @@
  */
 /** @type {module:express-promise-router} */
 const Router = require("express-promise-router");
-//const db = require("@saltcorn/data/db");
+const db = require("@saltcorn/data/db");
 const { error_catcher } = require("./utils.js");
 //const { mkTable, renderForm, link, post_btn } = require("@saltcorn/markup");
 const { getState } = require("@saltcorn/data/db/state");
@@ -534,26 +534,32 @@ router.post(
       { session: false },
       async function (err, user, info) {
         if (accessAllowedWrite(req, user, table)) {
-          const { _versions, ...row } = req.body || {};
-          const fields = table.getFields();
-          readState(row, fields, req);
-          const errors = await prepare_insert_row(row, fields);
-          if (errors.length > 0) {
-            getState().log(
-              2,
-              `API POST ${table.name} error: ${errors.join(", ")}`
+          db.withTransaction(async (client) => {
+            table.client = client;
+            const { _versions, ...row } = req.body || {};
+            const fields = table.getFields();
+            readState(row, fields, req);
+            const errors = await prepare_insert_row(row, fields);
+            if (errors.length > 0) {
+              getState().log(
+                2,
+                `API POST ${table.name} error: ${errors.join(", ")}`
+              );
+              res.status(400).json({ error: errors.join(", ") });
+              return;
+            }
+            const ins_res = await table.tryInsertRow(
+              row,
+              req.user || user || { role_id: 100 }
             );
-            res.status(400).json({ error: errors.join(", ") });
-            return;
-          }
-          const ins_res = await table.tryInsertRow(
-            row,
-            req.user || user || { role_id: 100 }
-          );
-          if (ins_res.error) {
-            getState().log(2, `API POST ${table.name} error: ${ins_res.error}`);
-            res.status(400).json(ins_res);
-          } else res.json(ins_res);
+            if (ins_res.error) {
+              getState().log(
+                2,
+                `API POST ${table.name} error: ${ins_res.error}`
+              );
+              res.status(400).json(ins_res);
+            } else res.json(ins_res);
+          });
         } else {
           getState().log(3, `API POST ${table.name} not authorized`);
           res.status(401).json({ error: req.__("Not authorized") });
@@ -585,26 +591,30 @@ router.post(
       { session: false },
       async function (err, user, info) {
         if (accessAllowedWrite(req, user, table)) {
-          try {
-            if (id === "undefined") {
-              const pk_name = table.pk_name;
-              //const fields = table.getFields();
-              const row = req.body || {};
-              //readState(row, fields);
-              await table.deleteRows(
-                { [pk_name]: row[pk_name] },
-                user || req.user || { role_id: 100 }
-              );
-            } else
-              await table.deleteRows(
-                { id },
-                user || req.user || { role_id: 100 }
-              );
-            res.json({ success: true });
-          } catch (e) {
-            getState().log(2, `API DELETE ${table.name} error: ${e.message}`);
-            res.status(400).json({ error: e.message });
-          }
+          db.withTransaction(async (client) => {
+            table.client = client;
+
+            try {
+              if (id === "undefined") {
+                const pk_name = table.pk_name;
+                //const fields = table.getFields();
+                const row = req.body || {};
+                //readState(row, fields);
+                await table.deleteRows(
+                  { [pk_name]: row[pk_name] },
+                  user || req.user || { role_id: 100 }
+                );
+              } else
+                await table.deleteRows(
+                  { id },
+                  user || req.user || { role_id: 100 }
+                );
+              res.json({ success: true });
+            } catch (e) {
+              getState().log(2, `API DELETE ${table.name} error: ${e.message}`);
+              res.status(400).json({ error: e.message });
+            }
+          });
         } else {
           getState().log(3, `API DELETE ${table.name} not authorized`);
           res.status(401).json({ error: req.__("Not authorized") });
@@ -636,28 +646,34 @@ router.post(
       { session: false },
       async function (err, user, info) {
         if (accessAllowedWrite(req, user, table)) {
-          const { _versions, ...row } = req.body || {};
-          const fields = table.getFields();
-          readState(row, fields, req);
-          const errors = await prepare_update_row(table, row, id);
-          if (errors.length > 0) {
-            getState().log(
-              2,
-              `API POST ${table.name} error: ${errors.join(", ")}`
+          db.withTransaction(async (client) => {
+            table.client = client;
+            const { _versions, ...row } = req.body || {};
+            const fields = table.getFields();
+            readState(row, fields, req);
+            const errors = await prepare_update_row(table, row, id);
+            if (errors.length > 0) {
+              getState().log(
+                2,
+                `API POST ${table.name} error: ${errors.join(", ")}`
+              );
+              res.status(400).json({ error: errors.join(", ") });
+              return;
+            }
+            const ins_res = await table.tryUpdateRow(
+              row,
+              id,
+              user || req.user || { role_id: 100 }
             );
-            res.status(400).json({ error: errors.join(", ") });
-            return;
-          }
-          const ins_res = await table.tryUpdateRow(
-            row,
-            id,
-            user || req.user || { role_id: 100 }
-          );
 
-          if (ins_res.error) {
-            getState().log(2, `API POST ${table.name} error: ${ins_res.error}`);
-            res.status(400).json(ins_res);
-          } else res.json(ins_res);
+            if (ins_res.error) {
+              getState().log(
+                2,
+                `API POST ${table.name} error: ${ins_res.error}`
+              );
+              res.status(400).json(ins_res);
+            } else res.json(ins_res);
+          });
         } else {
           getState().log(3, `API POST ${table.name} not authorized`);
           res.status(401).json({ error: req.__("Not authorized") });
@@ -690,21 +706,25 @@ router.delete(
       async function (err, user, info) {
         if (accessAllowedWrite(req, user, table)) {
           try {
-            if (id === "undefined") {
-              const pk_name = table.pk_name;
-              //const fields = table.getFields();
-              const row = req.body || {};
-              //readState(row, fields);
-              await table.deleteRows(
-                { [pk_name]: row[pk_name] },
-                user || req.user || { role_id: 100 }
-              );
-            } else
-              await table.deleteRows(
-                { [table.pk_name]: id },
-                user || req.user || { role_id: 100 }
-              );
-            res.json({ success: true });
+            db.withTransaction(async (client) => {
+              table.client = client;
+
+              if (id === "undefined") {
+                const pk_name = table.pk_name;
+                //const fields = table.getFields();
+                const row = req.body || {};
+                //readState(row, fields);
+                await table.deleteRows(
+                  { [pk_name]: row[pk_name] },
+                  user || req.user || { role_id: 100 }
+                );
+              } else
+                await table.deleteRows(
+                  { [table.pk_name]: id },
+                  user || req.user || { role_id: 100 }
+                );
+              res.json({ success: true });
+            });
           } catch (e) {
             getState().log(2, `API DELETE ${table.name} error: ${e.message}`);
             res.status(400).json({ error: e.message });
