afc170d088bed107ca901f5052aafab9628434ca
Tom Nielsen <tanielsen@gmail.com>
2026-03-26 21:56:56 +0000

    Autocomplete on various inputs. insert new menu items before admin pages. standard menu has notifications (#4033)
    * on fieldview
    
    * off if false
    
    * insert items added to menu before first admin page
    
    * standard menu has notifcations
    
    * fix standard menu types
    
    * fix some autocompletes on form

--- packages/saltcorn-data/base-plugin/types.ts
+++ packages/saltcorn-data/base-plugin/types.ts
@@ -1228,6 +1228,7 @@ const string = {
                   minlength: isdef(attrs.min_length) && attrs.min_length,
                   pattern: !!attrs.regexp && attrs.regexp,
                   autofocus: !!attrs.autofocus,
+                  autocomplete: attrs.autocomplete || undefined,
                   title:
                     !!attrs.re_invalid_error &&
                     !!attrs.regexp &&
@@ -1574,6 +1575,14 @@ const string = {
           label: "Visibility toggle",
           type: "Bool",
         },
+        {
+          name: "autocomplete",
+          label: "Autocomplete",
+          type: "String",
+          attributes: {
+            options: ["on", "off", "current-password", "new-password"],
+          },
+        },
       ],
       blockDisplay: true,
       description: "Password input type, characters are hidden when typed",
@@ -1595,6 +1604,10 @@ const string = {
           name: text_attr(nm),
           id: `input${text_attr(nm)}`,
           ...(isdef(v) && { value: text_attr(v) }),
+          autocomplete:
+            attrs?.autocomplete === false
+              ? "off"
+              : attrs?.autocomplete || undefined,
         });
         if (attrs?.visibility_toggle)
           return div(
